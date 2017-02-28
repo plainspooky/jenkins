bb3665dd6d4c367014c4f362e9d2045fb33f40fa
Giovanni Nunes <giovanni.nunes@gmail.com>
2017-02-28 16:12:10 -0300

    Vandalizações removidas

--- git-example/example.c
+++ git-example/example.c
@@ -10,7 +10,7 @@ int main(int argc, char **argv){
         printf("sem argumentos!\n");
         return 2;
     } else {
-        if ( num_params=1 ){
+        if ( num_params==1 ){
             printf("foi passado 1 argumento.\n");
         } else {
             printf("foram passados %i argumentos.\n", num_params);
